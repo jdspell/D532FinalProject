e1b95457e62ad2d5267df12be01459dcda1b0b5c
Jordan Spell <jdspell@iu.edu>
2023-07-02 10:33:56 -0400

    created intial file for connecting to postgres db for final project

---
+++ service/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, g
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+def get_db():
+    if 'db' not in g:
+        g.db = psycopg2.connect(
+            dbname=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASS'),
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT')
+        )
+    return g.db
+
+@app.teardown_appcontext
+def close_db(error):
+    if 'db' in g:
+        g.db.close()
+
+@app.route('/')
+def hello_world():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+    rows = cursor.fetchone()
+    return str(rows)
