cf5a507fd5d15ee2a33c960e11346acf39e04a03
Jordan Spell <jdspell@iu.edu>
2023-07-21 12:48:35 -0400

    updated app component to handle new search component

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,13 +1,22 @@
+'use client'
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
+
+import Search from './pages/Search';
 
 const Home = dynamic(() => import('./pages/Home'), { ssr: false });
 
 export default function App() {
+  const [data, setData] = useState([]);
+
   return (
-    <Home />
+    <div>
+      <Search setData={setData} />
+      <Home data={data} />
+    </div>
   )
 }
